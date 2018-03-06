eee1dee8ad13904efe81e72ff2dcf6c5f517a37a
tx-right-to-boycott <tx.right.to.boycott@gmail.com>
2018-03-05 22:25:55 -0600

    improvements to the navbar on mobile. submenu still wonky.

--- js/main.js
+++ js/main.js
@@ -18,11 +18,84 @@
         }
     }
 
+    var hideElement = function(elem) {
+
+      if (!hasClass(elem, 'hidden')) {
+        elem.className += ' hidden';
+      }
+    };
+
+    var showElement = function(elem) {
+      if (hasClass(elem, 'hidden')) {
+        elem.className = elem.className.replace('hidden', '').trim();
+      }
+    };
+
+    var displayElement = function(elem) {
+      showElement(elem);
+
+      if (!hasClass(elem, 'displayed')) {
+        elem.className += ' displayed';
+      }
+    };
+
     // Mobile nav function
     var mobileNav = document.querySelector('.nav-mobile');
     var toggle = document.querySelector('.nav-list');
     var navbar = document.querySelector('#navbar');
-    mobileNav.onclick = function () {
-        toggleClass(navbar, 'displayed');
+
+    var topLevelNavItems = document.querySelectorAll('.top-nav-only');
+    var submenuNav = document.querySelector('.dropdown-menu');
+    var submenuNavItems = document.querySelectorAll('.dropdown-item');
+    var submenuNavLinkNonMobile = document.querySelector('.navbar-dropdown-link');
+    var submenuNavLink = document.querySelector('.navbar-mobile-dropdown-link');
+    var submenuNavBackLink = document.querySelector('.navbar-mobile-dropdown-back-link');
+
+    var setUpInitialMenuState = function() {
+
+      displayElement(navbar);
+
+      topLevelNavItems.forEach(function(item) {
+        showElement(item, 'hidden');
+      });
+
+      hideElement(submenuNavLinkNonMobile);
+      showElement(submenuNavLink);
+      hideElement(submenuNav);
+
+      submenuNavItems.forEach(function(item) {
+        hideElement(item, 'hidden');
+      });
+
+      hideElement(submenuNavBackLink);
     };
+
+    mobileNav.onclick = function () {
+
+      if (hasClass(navbar, 'hidden') || !hasClass(navbar, 'displayed')) {
+        setUpInitialMenuState();
+      } else {
+        hideElement(navbar);
+      }
+    };
+
+    submenuNavLink.onclick = function() {
+
+      topLevelNavItems.forEach(function(item) {
+        hideElement(item);
+      });
+
+      hideElement(submenuNavLink);
+      showElement(submenuNav);
+
+      submenuNavItems.forEach(function(item) {
+        displayElement(item);
+      });
+
+      showElement(submenuNavBackLink);
+    }
+
+    submenuNavBackLink.onclick = function() {
+      setUpInitialMenuState();
+    }
 })();
